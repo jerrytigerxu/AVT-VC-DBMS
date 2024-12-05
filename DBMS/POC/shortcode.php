0f344cc29b28e54a5e032467a9d0c1fd5c599cd5
Jere Xu <jtxu2008@gmail.com>
2024-12-05 10:13:59 -0500

    Update shortcode.php

--- DBMS/POC/shortcode.php
+++ DBMS/POC/shortcode.php
@@ -1,11 +1,12 @@
 <?php
 
 function airtable_shortcode( $atts ) {
-  // Handling of the shortcode attributes (including table_name)
+  // 1. Handling of the shortcode attributes (including table_name)
   $atts = shortcode_atts( array(
         'base_id' => 'appmXtJKw9kGWHR0J', // Default Base ID
         'table_name' => 'Aspheric', // Default table name
-        'api_key' => 'patEV5dMy4CeyAbtR.7c11019e0a0eba7041bbc49ad0fd873b20015f8a9f0bc584344d3dac09a75b4f' // Default API key
+        'api_key' => 'patEV5dMy4CeyAbtR.7c11019e0a0eba7041bbc49ad0fd873b20015f8a9f0bc584344d3dac09a75b4f', // Default API key
+        'column_names' => '' // Attribute for column names
     ), $atts );
   
     $apiKey = $atts['api_key'];
@@ -13,7 +14,7 @@ function airtable_shortcode( $atts ) {
     $tableName = $atts['table_name'];
     $url = "https://api.airtable.com/v0/$baseId/$tableName";
 
-  // Data fetching from Airtable
+  // 2. Data fetching from Airtable
   $ch = curl_init($url);
     curl_setopt($ch, CURLOPT_HTTPHEADER, [
         'Authorization: Bearer ' . $apiKey,
@@ -26,63 +27,56 @@ function airtable_shortcode( $atts ) {
   // ******************  "$data" now contains a PHP array that was converted from the JSON data  **************
   $data = json_decode($response, true);
   
-  // Sort records by part number 
+  // 3. Sort records by part number 
   usort($data['records'], function($a, $b) {
       return strcmp($a['fields']['Part Number'], $b['fields']['Part Number']);
   });
   
-  // Gather all unique headers
-  $allHeaders = array();
-  foreach ($data['records'] as $record) {
+  // 4. Determine column names (unique headers or from direct attribute)
+  $columnNames = array();
+  if (!empty($atts['column_names'])) { // If column_names is provided
+      $columnNames = explode(',', $atts['column_names']);
+      $columnNames = array_map('trim', $columnNames);
+   } else {
+    // Gather all unique headers
+    foreach ($data['records'] as $record) {
       if (!empty($record['fields'])) {
-          $allHeaders = array_merge($allHeaders, array_keys($record['fields']));
+        $columnNames = array_merge($columnNames, array_keys($record['fields']));
       }
+    }
+    $columnNames = array_unique($columnNames);
   }
-  $allHeaders = array_unique($allHeaders); // prevent duplicate headers
   
   
-  
-  // Start creating the HTML table with the specified class and style
-  
+  // 5.  Build the HTML Table (using determined column names)
   $output = '<table class="table table-striped table-bordered table" style="margin: 0 auto; border: 1px Solid LightGray;">';
   $output .= '<thead><tr>';
 
-  $output .= '<th><h3><b>Part Number</b></h3></th>'; 
-
-  if (($key = array_search('Part Number', $allHeaders)) !== false) {
-      unset($allHeaders[$key]);
-  }
-
-  // Add remaining headers (bolded)
-  foreach ($allHeaders as $header) {
-      $output .= '<th><h3><b>' . $header . '</b></h3></th>'; 
+  // Add column headers (bolded) from $columnNames
+  foreach ($columnNames as $columnName) {
+    $output .= '<th><h3><b>' . $columnName . '</b></h3></th>'; 
   }
 
   $output .= '</tr></thead>';
   $output .= '<tbody>';
-  
-  // Populate table rows with data
+
   foreach ($data['records'] as $record) {
-      if (empty($record['fields']['Part Number'])) { 
-              continue; // Skip this row if it has no fields
-          }
-      $output .= '<tr>';
-      
-      $output .= '<td><h3>' . $record['fields']['Part Number'] . '</h3></td>'; 
-
-      // Add remaining cells in the correct order, format price if needed
-      foreach ($allHeaders as $header) {
-          $cellValue = isset($record['fields'][$header]) ? $record['fields'][$header] : ''; 
-          if (preg_match('/\*\*\$([\d.]+)\*\*/', $cellValue, $matches)) { 
-              $cellValue = '<b>$' . $matches[1] . '</b>';
-          }
-          $output .= '<td><h3>' . $cellValue . '</h3></td>'; 
-      }
-      $output .= '</tr>';
+    if (empty($record['fields']['Part Number'])) { 
+      continue;
     }
+    $output .= '<tr>';
 
-    $output .= '</tbody></table>';
+    // Add table cells based on $columnNames
+    foreach ($columnNames as $columnName) {
+      $cellValue = isset($record['fields'][$columnName]) ? $record['fields'][$columnName] : ''; 
+      if (preg_match('/\*\*\$([\d.]+)\*\*/', $cellValue, $matches)) { 
+        $cellValue = '<b>$' . $matches[1] . '</b>';
+      }
+      $output .= '<td><h3>' . $cellValue . '</h3></td>'; 
+    }
+    $output .= '</tr>';
+  }
 
-    return $output;
-  
+  $output .= '</tbody></table>';
+  return $output;
 }
