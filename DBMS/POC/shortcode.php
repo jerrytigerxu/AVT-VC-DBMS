a67b72a4dda386fd5ed404ff73b556440c7d6faf
Jere Xu <jtxu2008@gmail.com>
2024-12-05 09:42:00 -0500

    Update shortcode.php

--- DBMS/POC/shortcode.php
+++ DBMS/POC/shortcode.php
@@ -1,53 +1,83 @@
-function fetch_aspheric_lenses( $atts ) {
-      $apiKey = 'patD3Y6XZgfqKZPDZ.0efd5af39ddcc9bd64e229c36df3f44deeda8f3d09f92636372944e9f5d86198';
-      $baseId = 'appmXtJKw9kGWHR0J';
-      $tableName = 'Stock - Aspheric Lenses';
+<?php
 
-      $output = '';
-      $url = "https://api.airtable.com/v0/{$baseId}/{$tableName}";
-      $headers = array(
-      'Authorization' => 'Bearer ' . $apiKey,
-      );
+function airtable_shortcode( $atts ) {
+  $atts = shortcode_atts( array(
+        'base_id' => 'appmXtJKw9kGWHR0J', // Default Base ID
+        'table_name' => 'Aspheric', // Default table name
+        'api_key' => 'patEV5dMy4CeyAbtR.7c11019e0a0eba7041bbc49ad0fd873b20015f8a9f0bc584344d3dac09a75b4f' // Default API key
+    ), $atts );
+  
+    $apiKey = $atts['api_key'];
+    $baseId = $atts['base_id'];
+    $tableName = $atts['table_name'];
+    $url = "https://api.airtable.com/v0/$baseId/$tableName";
+  
+  $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, [
+        'Authorization: Bearer ' . $apiKey,
+        'Content-Type: application/json'
+    ]);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $response = curl_exec($ch);
+    curl_close($ch);
+  
+  	$data = json_decode($response, true);
+  
+  // Sort records by part number 
+    usort($data['records'], function($a, $b) {
+        return strcmp($a['fields']['Part Number'], $b['fields']['Part Number']);
+    });
+  
+  // Gather all unique headers
+    $allHeaders = array();
+    foreach ($data['records'] as $record) {
+        if (!empty($record['fields'])) {
+            $allHeaders = array_merge($allHeaders, array_keys($record['fields']));
+        }
+    }
+    $allHeaders = array_unique($allHeaders); 
+  
+  // Start creating the HTML table with the specified class and style
+  
+  $output = '<table class="table table-striped table-bordered table" style="margin: 0 auto; border: 1px Solid LightGray;">';
+    $output .= '<thead><tr>';
 
-      $ch = curl_init();
-      curl_setopt( $ch, CURLOPT_URL, $url );
-      curl_setopt( $ch, CURLOPT_HTTPHEADER, $headers );
-      curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
-	  $response = curl_exec( $ch );
+    $output .= '<th><h3><b>Part Number</b></h3></th>'; 
 
-      if ( curl_errno( $ch ) ) {
-        $output .= 'Error:' . curl_error( $ch );
-      } else {
-        $data = json_decode( $response, true );
+    if (($key = array_search('Part Number', $allHeaders)) !== false) {
+        unset($allHeaders[$key]);
+    }
 
-
-        /* Start building the table */
-        $output .= '<table style="border-collapse: collapse; width: 100%;">';
-        $output .= '<thead>';
+    // Add remaining headers (bolded)
+    foreach ($allHeaders as $header) {
+        $output .= '<th><h3><b>' . $header . '</b></h3></th>'; 
+    }
+  
+    $output .= '</tr></thead>';
+    $output .= '<tbody>';
+  
+  // Populate table rows with data
+  foreach ($data['records'] as $record) {
+        if (empty($record['fields']['Part Number'])) { 
+                continue; // Skip this row if it has no fields
+            }
         $output .= '<tr>';
+        
+        $output .= '<td><h3>' . $record['fields']['Part Number'] . '</h3></td>'; 
 
-        /* Dynamically get field names for table headers */
-        if (isset($data['records'][0]['fields'])) {
-          foreach (array_keys($data['records'][0]['fields']) as $fieldName) {
-            $output .= '<th style="border: 1px solid black; padding: 8px;">' . $fieldName . '</th>';
-          }
+        // Add remaining cells in the correct order, format price if needed
+        foreach ($allHeaders as $header) {
+            $cellValue = isset($record['fields'][$header]) ? $record['fields'][$header] : ''; 
+            if (preg_match('/\*\*\$([\d.]+)\*\*/', $cellValue, $matches)) { 
+                $cellValue = '<b>$' . $matches[1] . '</b>';
+            }
+            $output .= '<td><h3>' . $cellValue . '</h3></td>'; 
         }
         $output .= '</tr>';
-        $output .= '</thead>';
-        $output .= '<tbody>';
+    }
 
-        /* Fill out the data for the records (rows x columns) */
-        foreach ( $data['records'] as $record ) {
-          $output .= '<tr>';
-          foreach ($record['fields'] as $fieldValue) {
-            $output .= '<td style="border: 1px solid black; padding: 8px;">' . $fieldValue . '</td>';
-          }
-          $output .= '</tr>';
-        }
-        $output .= '</tbody>';
-        $output .= '</table>';
-    }
-    curl_close( $ch );
+    $output .= '</tbody></table>';
+
     return $output;
-    }
-    add_shortcode( 'aspheric_lenses', 'fetch_aspheric_lenses' );
+  
+}
