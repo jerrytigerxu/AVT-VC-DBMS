cee84f39e412ce380314ebcddd6c469cc1a8bc0e
Jere Xu <jtxu2008@gmail.com>
2024-12-05 09:48:53 -0500

    Update shortcode.php

--- DBMS/POC/shortcode.php
+++ DBMS/POC/shortcode.php
@@ -20,60 +20,63 @@ function airtable_shortcode( $atts ) {
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     $response = curl_exec($ch);
     curl_close($ch);
-  
-  	$data = json_decode($response, true);
+
+  // ******************  All of the necessary JSON data is in "$data"  **************
+  $data = json_decode($response, true);
   
   // Sort records by part number 
-    usort($data['records'], function($a, $b) {
-        return strcmp($a['fields']['Part Number'], $b['fields']['Part Number']);
-    });
+  usort($data['records'], function($a, $b) {
+      return strcmp($a['fields']['Part Number'], $b['fields']['Part Number']);
+  });
   
   // Gather all unique headers
-    $allHeaders = array();
-    foreach ($data['records'] as $record) {
-        if (!empty($record['fields'])) {
-            $allHeaders = array_merge($allHeaders, array_keys($record['fields']));
-        }
-    }
-    $allHeaders = array_unique($allHeaders); 
+  $allHeaders = array();
+  foreach ($data['records'] as $record) {
+      if (!empty($record['fields'])) {
+          $allHeaders = array_merge($allHeaders, array_keys($record['fields']));
+      }
+  }
+  $allHeaders = array_unique($allHeaders); // prevent duplicate headers
+  
+  
   
   // Start creating the HTML table with the specified class and style
   
   $output = '<table class="table table-striped table-bordered table" style="margin: 0 auto; border: 1px Solid LightGray;">';
-    $output .= '<thead><tr>';
+  $output .= '<thead><tr>';
 
-    $output .= '<th><h3><b>Part Number</b></h3></th>'; 
+  $output .= '<th><h3><b>Part Number</b></h3></th>'; 
 
-    if (($key = array_search('Part Number', $allHeaders)) !== false) {
-        unset($allHeaders[$key]);
-    }
+  if (($key = array_search('Part Number', $allHeaders)) !== false) {
+      unset($allHeaders[$key]);
+  }
 
-    // Add remaining headers (bolded)
-    foreach ($allHeaders as $header) {
-        $output .= '<th><h3><b>' . $header . '</b></h3></th>'; 
-    }
-  
-    $output .= '</tr></thead>';
-    $output .= '<tbody>';
+  // Add remaining headers (bolded)
+  foreach ($allHeaders as $header) {
+      $output .= '<th><h3><b>' . $header . '</b></h3></th>'; 
+  }
+
+  $output .= '</tr></thead>';
+  $output .= '<tbody>';
   
   // Populate table rows with data
   foreach ($data['records'] as $record) {
-        if (empty($record['fields']['Part Number'])) { 
-                continue; // Skip this row if it has no fields
-            }
-        $output .= '<tr>';
-        
-        $output .= '<td><h3>' . $record['fields']['Part Number'] . '</h3></td>'; 
+      if (empty($record['fields']['Part Number'])) { 
+              continue; // Skip this row if it has no fields
+          }
+      $output .= '<tr>';
+      
+      $output .= '<td><h3>' . $record['fields']['Part Number'] . '</h3></td>'; 
 
-        // Add remaining cells in the correct order, format price if needed
-        foreach ($allHeaders as $header) {
-            $cellValue = isset($record['fields'][$header]) ? $record['fields'][$header] : ''; 
-            if (preg_match('/\*\*\$([\d.]+)\*\*/', $cellValue, $matches)) { 
-                $cellValue = '<b>$' . $matches[1] . '</b>';
-            }
-            $output .= '<td><h3>' . $cellValue . '</h3></td>'; 
-        }
-        $output .= '</tr>';
+      // Add remaining cells in the correct order, format price if needed
+      foreach ($allHeaders as $header) {
+          $cellValue = isset($record['fields'][$header]) ? $record['fields'][$header] : ''; 
+          if (preg_match('/\*\*\$([\d.]+)\*\*/', $cellValue, $matches)) { 
+              $cellValue = '<b>$' . $matches[1] . '</b>';
+          }
+          $output .= '<td><h3>' . $cellValue . '</h3></td>'; 
+      }
+      $output .= '</tr>';
     }
 
     $output .= '</tbody></table>';
